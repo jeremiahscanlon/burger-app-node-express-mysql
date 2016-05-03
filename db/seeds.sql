f6b14fedd347477da5b6be17a578078982f3f173
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-05-03 01:20:55 -0400

    file system setup

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured) Values
+	(
+		name: 'Double Seceret Probation',
+		devoured: false
+	),(
+		name: 'Mile High Jalapenos',
+		devoured: false
+	),(
+		name: 'Buffalo Bills Bonanza',
+		devoured: true
+	);
